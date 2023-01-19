93e6bcf646681f11a1192e3208cb53a22a57db03
DerryAlex <derry.alex@foxmail.com>
2023-01-19 16:08:03 +0800

    support internal child for template.bindChild

--- generate/output/template.zig
+++ generate/output/template.zig
@@ -5,13 +5,13 @@ const Gtk = @import("Gtk.zig");
 const core = Gtk.core;
 
 /// Convenience wrapper for gtk_widget_class_bind_template_child
-/// Template child should be named `"TC" ++ name`
+/// Template child should be named `"TC" ++ name` or `"TI" ++ name`(internal child)
 pub fn bindChild(class: *Gtk.WidgetClass, comptime Impl: type) void {
     inline for (comptime meta.fieldNames(Impl)) |name| {
-        if (comptime name.len <= 2 or name[0] != 'T' or name[1] != 'C') continue;
+        if (comptime name.len <= 2 or name[0] != 'T' or (name[1] != 'C' and name[1] != 'T')) continue;
         comptime var name_c: [name.len - 2:0]u8 = undefined;
         comptime std.mem.copy(u8, name_c[0..], name[2..]);
-        class.bindTemplateChildFull(&name_c, core.Boolean.False, @offsetOf(Impl, name));
+        class.bindTemplateChildFull(&name_c, if (name[1] == 'T') core.Boolean.True else core.Boolean.False, @offsetOf(Impl, name));
     }
 }
 
