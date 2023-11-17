e49f7b4cce92b4ca68584eb55fd50049c9470ae4
Derry Alex <derryalex@foxmail.com>
2023-11-17 06:06:50 -0800

    update build proc

--- build.zig
+++ build.zig
@@ -4,15 +4,24 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const exe = b.addExecutable(.{ .name = "main", .root_source_file = .{ .path = "src/main.zig" }, .optimize = optimize, .target = target });
+    const exe = b.addExecutable(.{
+        .name = "main",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
     exe.linkLibC();
     exe.linkSystemLibrary("gobject-introspection-1.0");
     b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Generate the binding");
+    run_step.dependOn(&run_exe.step);
 
     const tests = b.addTest(.{ .root_source_file = .{ .path = "src/gir.zig" } });
     tests.linkLibC();
     tests.linkSystemLibrary("gobject-introspection-1.0");
 
-    const test_step = b.step("test", "Run tests");
+    const test_step = b.step("test_gir", "Test gir.zig");
     test_step.dependOn(&tests.step);
 }
