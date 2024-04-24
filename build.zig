4745a8f848394142e3f0912984bcebe3eabc8c76
DerryAlex <derry.alex@foxmail.com>
2024-04-24 16:01:33 +0800

    update version to 0.8.0

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.Build) !void {
     const xml = b.dependency("xml", .{});
 
     const options = b.addOptions();
-    const version = b.option([]const u8, "version", "application version string") orelse "0.7.1";
+    const version = b.option([]const u8, "version", "application version string") orelse "0.8.0";
     options.addOption([]const u8, "version", version);
 
     const exe = b.addExecutable(.{
