7073f24ad59809a1cc64bf3a5ccffe5fb6615437
DerryAlex <derry.alex@foxmail.com>
2024-06-06 12:07:54 +0800

    update scripts

--- lib/tools/generate.sh
+++ lib/tools/generate.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
-for gir in cairo-1.0 freetype2-2.0 Gdk-4.0 GdkPixbuf-2.0 Gio-2.0 GLib-2.0 GModule-2.0 GObject-2.0 Graphene-1.0 Gsk-4.0 Gtk-4.0 HarfBuzz-0.0 Pango-1.0 PangoCairo-1.0
+compiler=`pwd`/gobject-introspection/build/tools/g-ir-compiler
+typelibdir=`pwd`/../girepository-1.0
+
+#girdir=/usr/share/gir-1.0
+girdir=gir-files
+cd ${girdir}
+
+for gir in `ls *.gir`
 do
-    ./g-ir-compiler /usr/share/gir-1.0/${gir}.gir -o ../girepository-1.0/${gir}.typelib
+    typelib=`echo ${gir} | sed 's/.gir/.typelib/'`
+    ${compiler} ${gir} -o ${typelibdir}/${typelib} --includedir .
 done
