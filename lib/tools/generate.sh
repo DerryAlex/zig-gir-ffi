718f217b7d6a72289eeae6f44b6419cfdeed5e17
DerryAlex <derry.alex@foxmail.com>
2024-06-06 15:55:17 +0800

    script: prefer $(...) over `...`

--- lib/tools/generate.sh
+++ lib/tools/generate.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
-compiler=`pwd`/gobject-introspection/build/tools/g-ir-compiler
-typelibdir=`pwd`/../girepository-1.0
+compiler=$(pwd)/gobject-introspection/build/tools/g-ir-compiler
+typelibdir=$(pwd)/../girepository-1.0
 
 #girdir=/usr/share/gir-1.0
 girdir=gir-files
 cd ${girdir}
 
-for gir in `ls *.gir`
+for gir in $(ls *.gir)
 do
-    typelib=`echo ${gir} | sed 's/.gir/.typelib/'`
+    typelib=$(echo ${gir} | sed 's/.gir/.typelib/')
     ${compiler} ${gir} -o ${typelibdir}/${typelib} --includedir .
 done
