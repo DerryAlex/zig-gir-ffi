589799b56010e2c9ecd3f71ba9edd83ad94c15e5
DerryAlex <derry.alex@foxmail.com>
2024-07-22 13:46:42 +0800

    handle the chaos of utf8

--- lib/girepository-1.0/ci.sh
+++ lib/girepository-1.0/ci.sh
@@ -12,9 +12,18 @@ cd ../..
 
 git clone https://github.com/gtk-rs/gir-files.git && cd gir-files
 git checkout ${gir_version}
+# utf8
 sed -i 's/type name="utf8" c:type="gchar"/type name="gchar" c:type="gchar"/g' GLib-2.0.gir
-sed -i 's/field name="priority" introspectable="0" readable="0" private="1"/field name="priority" readable="0" private="1"/g' GLib-2.0.gir
-sed -i 's/enumeration name="ThreadPriority" introspectable="0" deprecated="1"/enumeration name="ThreadPriority" deprecated="1"/g' GLib-2.0.gir
+sed -i 's#<type name="utf8" c:type="gchar\*\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"> </array>#g' GLib-2.0.gir
+sed -i 's#<type name="utf8" c:type="const gchar\*\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"> </array>#g' GLib-2.0.gir
+sed -i 's#<type name="utf8" c:type="const gchar\* const\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"> </array>#g' GLib-2.0.gir
+sed -i 's#<type name="utf8" c:type="const char\* const\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"> </array>#g' GLib-2.0.gir
+sed -i 's/type name="utf8" c:type="char"/type name="gchar" c:type="char"/g' HarfBuzz-0.0.gir
+sed -i 's#<type name="utf8" c:type="const char\*\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"> </array>#g' HarfBuzz-0.0.gir
+# introspectable="0"
+sed -i 's/field name="priority" introspectable="0"/field name="priority"/g' GLib-2.0.gir
+sed -i 's/enumeration name="ThreadPriority" introspectable="0" deprecated="1"/enumeration name="ThreadPriority"/g' GLib-2.0.gir
+# misc
 sed -i 's/gconstpointer/gpointer/g' Pango-1.0.gir
 for gir in $(ls *.gir)
 do
