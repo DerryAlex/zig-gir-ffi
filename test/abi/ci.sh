abcad1c7cce90de8f5e4050c0e4368488ca10633
DerryAlex <derry.alex@foxmail.com>
2024-07-22 11:10:35 +0800

    patch c_linux.zig

--- test/abi/ci.sh
+++ test/abi/ci.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 pkg_rel=1 # used to trigger ci without actual changes
 zig translate-c -cflags $(pkg-config --cflags gtk4) -- /usr/include/gtk-4.0/gtk/gtk.h -I/usr/include -I/usr/include/x86_64-linux-gnu/ >c_linux.zig
+patch c_linux.zig c_linux.patch
