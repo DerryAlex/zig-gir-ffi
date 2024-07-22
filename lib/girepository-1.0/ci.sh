b4fa19e631134a9b5a96e4a1941fc3ece0b149ba
DerryAlex <derry.alex@foxmail.com>
2024-07-22 09:40:44 +0800

    update gir files

--- lib/girepository-1.0/ci.sh
+++ lib/girepository-1.0/ci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-gir_version="0.19.2"
+gir_version="0.20.0"
 
 apt-get source gobject-introspection
 cd $(ls -F | grep 'gobject-introspection' | grep '/$')
@@ -12,6 +12,7 @@ cd ../..
 
 git clone https://github.com/gtk-rs/gir-files.git && cd gir-files
 git checkout ${gir_version}
+sed -i 's/type name="utf8" c:type="gchar"/type name="gchar" c:type="gchar"/g' GLib-2.0.gir
 sed -i 's/gconstpointer/gpointer/g' Pango-1.0.gir
 for gir in $(ls *.gir)
 do
