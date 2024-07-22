d63e5c7cd8db29b2287e3413c20403f874fffc25
DerryAlex <derry.alex@foxmail.com>
2024-07-22 14:40:30 +0800

    fix invalid element type

--- lib/girepository-1.0/ci.sh
+++ lib/girepository-1.0/ci.sh
@@ -14,12 +14,12 @@ git clone https://github.com/gtk-rs/gir-files.git && cd gir-files
 git checkout ${gir_version}
 # utf8
 sed -i 's/type name="utf8" c:type="gchar"/type name="gchar" c:type="gchar"/g' GLib-2.0.gir
-sed -i 's#<type name="utf8" c:type="gchar\*\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"/> </array>#g' GLib-2.0.gir
-sed -i 's#<type name="utf8" c:type="const gchar\*\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"/> </array>#g' GLib-2.0.gir
-sed -i 's#<type name="utf8" c:type="const gchar\* const\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"/> </array>#g' GLib-2.0.gir
-sed -i 's#<type name="utf8" c:type="const char\* const\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"/> </array>#g' GLib-2.0.gir
+sed -i '237,$s#<type name="utf8" c:type="gchar\*\*"/>#<array c:type="const char \*\*"> <type name="utf8"/> </array>#g' GLib-2.0.gir
+sed -i 's#<type name="utf8" c:type="const gchar\*\*"/>#<array c:type="const char \*\*"> <type name="utf8"/> </array>#g' GLib-2.0.gir
+sed -i 's#<type name="utf8" c:type="const gchar\* const\*"/>#<array c:type="const char \*\*"> <type name="utf8"/> </array>#g' GLib-2.0.gir
+sed -i 's#<type name="utf8" c:type="const char\* const\*"/>#<array c:type="const char \*\*"> <type name="utf8"/> </array>#g' GLib-2.0.gir
 sed -i 's/type name="utf8" c:type="char"/type name="gchar" c:type="char"/g' HarfBuzz-0.0.gir
-sed -i 's#<type name="utf8" c:type="const char\*\*"/>#<array c:type="const char \*\*"> <type name="utf8" c:type="char\*"/> </array>#g' HarfBuzz-0.0.gir
+sed -i 's#<type name="utf8" c:type="const char\*\*"/>#<array c:type="const char \*\*"> <type name="utf8"/> </array>#g' HarfBuzz-0.0.gir
 # introspectable="0"
 sed -i 's/field name="priority" introspectable="0"/field name="priority"/g' GLib-2.0.gir
 sed -i 's/enumeration name="ThreadPriority" introspectable="0" deprecated="1"/enumeration name="ThreadPriority"/g' GLib-2.0.gir
