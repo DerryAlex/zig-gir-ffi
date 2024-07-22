cfd93944d5a9cc02488cd43a66c4cff5ee23b5ab
DerryAlex <derry.alex@foxmail.com>
2024-07-22 11:33:02 +0800

    make Thread.priority introspectable

--- lib/girepository-1.0/ci.sh
+++ lib/girepository-1.0/ci.sh
@@ -13,6 +13,7 @@ cd ../..
 git clone https://github.com/gtk-rs/gir-files.git && cd gir-files
 git checkout ${gir_version}
 sed -i 's/type name="utf8" c:type="gchar"/type name="gchar" c:type="gchar"/g' GLib-2.0.gir
+sed -i 's/field name="priority" introspectable="0" readable="0" private="1"/field name="priority" readable="0" private="1"/g' GLib-2.0.gir
 sed -i 's/enumeration name="ThreadPriority" introspectable="0" deprecated="1"/enumeration name="ThreadPriority" deprecated="1"/g' GLib-2.0.gir
 sed -i 's/gconstpointer/gpointer/g' Pango-1.0.gir
 for gir in $(ls *.gir)
