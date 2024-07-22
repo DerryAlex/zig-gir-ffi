ae19a2dd669de9cbe70a81d11c994d3794f593de
DerryAlex <derry.alex@foxmail.com>
2024-07-22 10:52:20 +0800

    track c.zig with lfs and trigger ci

--- test/abi/ci.sh
+++ test/abi/ci.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-pkg_rel=0 # used to trigger ci without actual changes
+pkg_rel=1 # used to trigger ci without actual changes
 zig translate-c -cflags $(pkg-config --cflags gtk4) -- /usr/include/gtk-4.0/gtk/gtk.h -I/usr/include -I/usr/include/x86_64-linux-gnu/ >c_linux.zig
