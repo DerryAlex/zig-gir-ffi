af802cfe70f103113a160cb18b9a90061b42d334
DerryAlex <derry.alex@foxmail.com>
2024-08-12 11:50:42 +0800

    try to run zig in msys2 env

--- test/abi/ci.bat
+++ test/abi/ci.bat
@@ -1,3 +1,2 @@
-:: DISABLED
-:: zig translate-c -cflags $(pkg-config --cflags gtk4) -- c_win.h -I/ucrt64/include >c_win.zig
-:: patch c_win.zig c_win.patch
+msys2 -c 'zig translate-c -cflags $(pkg-config --cflags gtk4) -- c_win.h -I/ucrt64/include >c_win.zig'
+patch c_win.zig c_win.patch
