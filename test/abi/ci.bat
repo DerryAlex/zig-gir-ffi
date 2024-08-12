ae03997ad205bed35b43fff0c5b4bcd3731f07dc
DerryAlex <derry.alex@foxmail.com>
2024-08-12 12:00:27 +0800

    fix zig path on msys2 env

--- test/abi/ci.bat
+++ test/abi/ci.bat
@@ -1,2 +1,2 @@
-msys2 -c 'zig translate-c -cflags $(pkg-config --cflags gtk4) -- c_win.h -I/ucrt64/include >c_win.zig'
+msys2 -c '/clang64/bin/zig translate-c -cflags $(pkg-config --cflags gtk4) -- c_win.h -I/ucrt64/include >c_win.zig'
 patch c_win.zig c_win.patch
