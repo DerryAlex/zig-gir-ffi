25d18324272edc83595bf5fb4e5297f2d66b1acc
DerryAlex <derry.alex@foxmail.com>
2024-08-12 11:25:29 +0800

    [skip ci] disable win ci

--- test/abi/ci.bat
+++ test/abi/ci.bat
@@ -1,2 +1,3 @@
-zig translate-c -cflags $(pkg-config --cflags gtk4) -- c_win.h -I/ucrt64/include >c_win.zig
+:: DISABLED
+:: zig translate-c -cflags $(pkg-config --cflags gtk4) -- c_win.h -I/ucrt64/include >c_win.zig
 :: patch c_win.zig c_win.patch
