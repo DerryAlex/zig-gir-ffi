693d1a3686eca5dfdcca94302e7fe9197c97fa53
DerryAlex <derry.alex@foxmail.com>
2024-10-29 15:57:58 +0800

    remove cimport ci

--- test/abi/c_linux.zig
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aab6e7b252beb7bd03466e9c5d4425a304dea8fad0c7c2533dde12048ab79872
-size 4813958
--- test/abi/c_win.zig
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9691b4707d10796bf926862096fe8b850691002445c5e1aa934564dea7ceb116
-size 11637089
