9abc92f2832fdefcbc1f02ffcc35f93dc1917e2d
DerryAlex <derry.alex@foxmail.com>
2024-11-02 16:54:32 +0800

    revert to gi-1.0 for typelib generation

--- lib/girepository-1.0/ci.sh
+++ lib/girepository-1.0/ci.sh
@@ -2,13 +2,16 @@
 
 gir_version="0.20.1"
 
-apt-get source glib2.0
-cd $(ls -F | grep 'glib2.0' | grep '/$')
+# apt-get source glib2.0
+# cd $(ls -F | grep 'glib2.0' | grep '/$')
+apt-get source gobject-introspection
+cd $(ls -F | grep 'gobject-introspection' | grep '/$')
 patch girepository/girnode.c ../girnode.patch || exit 1
 meson setup build && cd build
 meson compile
-export PATH=$(pwd)/girepository/compiler:$(pwd)/girepository/decompiler:$(pwd)/girepository/inspector:${PATH}
+# export PATH=$(pwd)/girepository/compiler:$(pwd)/girepository/decompiler:$(pwd)/girepository/inspector:${PATH}
 cd ../..
+export PATH=$(pwd)/tools:${PATH}
 
 git clone https://github.com/gtk-rs/gir-files.git && cd gir-files
 git checkout ${gir_version}
@@ -32,7 +35,8 @@ sed -i 's/gconstpointer/gpointer/g' Pango-1.0.gir
 for gir in $(ls *.gir)
 do
     typelib=$(echo ${gir} | sed 's/.gir/.typelib/')
-    gi-compile-repository ${gir} -o ../${typelib} --includedir .
+    # gi-compile-repository ${gir} -o ../${typelib} --includedir .
+    g-ir-compiler ${gir} -o ../${typelib} --includedir .
 done
 
 cd ..
